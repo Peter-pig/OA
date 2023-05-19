c22e192a3e4759da9ee6b17965ab2e235baa4b69
peter_pig <683@qq.com>
2023-05-19 10:39:36 +0800

    变更

--- OaBusiness/Oa_Common/src/main/java/com/example/commom/Utils/JwtUtils.java
+++ OaBusiness/Oa_Common/src/main/java/com/example/commom/Utils/JwtUtils.java
@@ -12,22 +12,20 @@ public class JwtUtils {
     private static final long expirationTime = 3600000; // 1 hour in milliseconds
 
     public static String generateToken(String subject) {
-        String jwt = Jwts.builder()
+        return Jwts.builder()
                 .setSubject(subject)
                 .setExpiration(new Date(System.currentTimeMillis() + expirationTime))
                 .signWith(key)
                 .compact();
-        return jwt;
     }
 
     public static Claims decodeToken(String token) {
         try {
-            Claims claims = Jwts.parserBuilder()
+            return Jwts.parserBuilder()
                     .setSigningKey(key)
                     .build()
                     .parseClaimsJws(token)
                     .getBody();
-            return claims;
         } catch (JwtException e) {
             throw new RuntimeException("Invalid token.");
         }
